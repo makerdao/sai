90deeb3886d6ddb2abd7ccfc2f1f1b356725f72b
Nikolai Mushegian <nikolai@nexusdev.us>
2017-03-25 15:57:56 -0400

    sketch

--- src/test.sol
+++ src/test.sol
@@ -1,13 +1,25 @@
 pragma solidity ^0.4.8;
 
 import "ds-test/test.sol";
+
+import 'ds-token/token.sol';
 import './yas.sol';
 
 contract Test is DSTest {
     YAS yas;
+    DSToken _col;
     function setUp() {
-        yas = new YAS();
+        _col = new DSToken("collateral", "COL", 18);
+        _col.mint(100 ether);
+        yas = new YAS(_col);
+        _col.approve(yas, 100000 ether);
     }
-    function testFail() {
+    function testJoinExit() {
+        // edge case
+        yas.join(10 ether);
+        assertEq( yas.SAY().balanceOf(this), 10 ether );
+        // price formula 
+        yas.join(10 ether); 
+        assertEq( yas.SAY().balanceOf(this), 20 ether );
     }
 }
--- src/yas.sol
+++ src/yas.sol
@@ -4,41 +4,69 @@ import "ds-auth/auth.sol";
 import "ds-token/token.sol";
 
 contract YAS is DSAuth {
-    DSToken _yas; // stablecoin
-    DSToken _say; // claims on collateral
-    DSToken _col; // collateral e.g. ETH
+    struct Cup {
+        address lad;
+    }
+    DSToken _col;
+    function COL() returns (ERC20) {
+        return _col;
+    }
+    DSToken _say;
+    function SAY() returns (ERC20) {
+        return _say;
+    } 
+    DSToken _yas;
+    function YAS() returns (ERC20) {
+        return _yas;
+    } 
 
-    bytes32 _ref; // description of reference asset (e.g. "USD")
-    uint256 _wut; // _col:ref price TODO use feedbase
-
-    uint256 _mat; // liquidation ratio
-    uint256 _fee; // fee/decay
-    uint256 _chi; // debt unit converter
-    struct CCP {
-        address lad; // owner
-        uint256 say; // claims
-        uint256 rum; // debt unit
+    function YAS(DSToken col) {
+        _col = col;
+        _say = new DSToken("SAY", "SAY", 18);
+        _yas = new DSToken("YAS", "YAS", 18);
     }
 
     // COL <-> SAY
-    function join(uint256 amt) {}
-    function exit(uint256 amt) {}
+    function join(uint128 amt) {
+        uint128 price;
+        // this avoids 0 edge case / rounding errors
+        // TODO delegate edge case via fee built into conversion formula
+        if( _col.balanceOf(this) < 1 ether ) {
+            price = 1;
+        } else {
+            price = uint128(_col.balanceOf(this) / _say.totalSupply());
+        }
+        var prize = amt * price;
+        assert( _col.pull(msg.sender, amt) );
+        _say.mint(prize);
+        _say.push(msg.sender, prize);
+    }
+    function exit(uint128 amt) {
+        var price = uint128(_col.balanceOf(this) / _say.totalSupply());
+            // TODO use formulas, delegate edge case via fee
+        var prize = amt * price; // prize for contract
+        assert( _say.pull(msg.sender, prize ) );
+        assert( _col.push(msg.sender, amt) );
+    }
 
     // CDP ops
     function open() returns (uint256 urn) {}
-    function shut() returns (uint256 urn) {}
-    function lock(uint256 urn, uint256 amt) {}
-    function free(uint256 urn, uint256 amt) {}
-    function draw(uint256 urn, uint256 amt) {}
-    function wipe(uint256 urn, uint256 amt) {}
+    function shut(uint256 urn) {}
+    // lock/free SAY tokens
+    function lock(uint256 urn, uint128 amt) {}
+    function free(uint256 urn, uint128 amt) {}
+    // draw/wipe YAS tokens
+    function draw(uint256 urn, uint128 amt) {}
+    function wipe(uint256 urn, uint128 amt) {}
 
     // keeper
+    // REF:COL price
     function tell(uint256 wut) {}
     function bite(uint256 urn) {}
 
     // auto MM
-    function boom(uint256 amt) {}
-    function bust(uint256 amt) {}
+    function boom(uint128 amt) {}
+    function bust(uint128 amt) {}
 
     // settle backdoor
     function kill(uint256 wut) {}
