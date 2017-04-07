ee40cbed2a53b4d0658d1dbcc2eae4dd55a9ae56
rain <rainbreak@riseup.net>
2017-04-07 16:45:29 +0100

    simple auth

--- src/tub.sol
+++ src/tub.sol
@@ -99,7 +99,7 @@ contract Tub is DSThing {
     // This is nearly the equivalent of biting all cups at once.
     // Important consideration: the gems associated with free skr can
     // be tapped to make sai whole.
-    function kill(uint128 price) note authorized("kill") {
+    function kill(uint128 price) note auth {
         off = true;
         // take on the debt
         pot.push(sin, this);
@@ -160,13 +160,13 @@ contract Tub is DSThing {
         delete cups[cup];
     }
 
-    function chop(uint128 ray) note authorized("mold") {
+    function chop(uint128 ray) note auth) {
         axe = ray;
     }
-    function cork(uint128 wad) note authorized("mold") {
+    function cork(uint128 wad) note auth {
         hat = wad;
     }
-    function cuff(uint128 ray) note authorized("mold") {
+    function cuff(uint128 ray) note auth {
         mat = ray;
     }
 
