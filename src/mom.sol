bfea779d5b45cb7075796fbce620268c92146cc7
rain <rainbreak@riseup.net>
2017-05-18 22:43:24 +0100

    roles: adapt to sai. admin / user

--- src/mom.sol
+++ src/mom.sol
@@ -5,25 +5,30 @@ import "ds-roles/roles.sol";
 contract SaiMom is DSRoles {
     // role identifiers
     uint8 public admin  = 0;
-    uint8 public issuer = 1;
-    uint8 public holder = 2;
+    uint8 public user = 1;
 
     function SaiMom(address target) {
         // == admin
-        setRoleCapability(admin, target, sig("register(address)"), true);
-        setRoleCapability(admin, target, sig("setVault(uint48,address)"), true);
-        setRoleCapability(admin, target, sig("setFeed(uint48,bytes12)"), true);
-        setRoleCapability(admin, target, sig("setSpread(uint48,uint256)"), true);
-        setRoleCapability(admin, target, sig("setCeiling(uint48,uint256)"), true);
-        setRoleCapability(admin, target, sig("unregister(uint48)"), true);
+        setRoleCapability(admin, target, sig("chop(uint128)"), true);
+        setRoleCapability(admin, target, sig("cork(uint128)"), true);
+        setRoleCapability(admin, target, sig("cuff(uint128)"), true);
+        setRoleCapability(admin, target, sig("cage(uint128)"), true);
 
-        // == issuer
-        setRoleCapability(issuer, target, sig("issue(uint48,uint256)"), true);
-        setRoleCapability(issuer, target, sig("cover(uint48,uint256)"), true);
-
-        // == holder
-        setRoleCapability(holder, target, sig("transfer(address,uint256)"), true);
-        setRoleCapability(holder, target, sig("transferFrom(address,address,uint256)"), true);
+        // == user
+        setRoleCapability(user, target, sig("join(uint128)"), true);
+        setRoleCapability(user, target, sig("exit(uint128)"), true);
+        setRoleCapability(user, target, sig("open()"), true);
+        setRoleCapability(user, target, sig("shut(bytes32)"), true);
+        setRoleCapability(user, target, sig("lock(bytes32,uint128)"), true);
+        setRoleCapability(user, target, sig("free(bytes32,uint128)"), true);
+        setRoleCapability(user, target, sig("draw(bytes32,uint128)"), true);
+        setRoleCapability(user, target, sig("wipe(bytes32,uint128)"), true);
+        setRoleCapability(user, target, sig("give(bytes32,address)"), true);
+        setRoleCapability(user, target, sig("bite(bytes32)"), true);
+        setRoleCapability(user, target, sig("boom(uint128)"), true);
+        setRoleCapability(user, target, sig("bust(uint128)"), true);
+        setRoleCapability(user, target, sig("cash()"), true);
+        setRoleCapability(user, target, sig("bail(bytes32)"), true);
     }
 
     function sig(string name) constant returns (bytes4) {
@@ -32,34 +37,23 @@ contract SaiMom is DSRoles {
 
     function addAdmin(address who) auth {
         setUserRole(who, admin, true);
-        setUserRole(who, issuer, true);
-        setUserRole(who, holder, true);
+        setUserRole(who, user, true);
     }
-    function addIssuer(address who) auth {
-        setUserRole(who, issuer, true);
-        setUserRole(who, holder, true);
-    }
-    function addHolder(address who) auth {
-        setUserRole(who, holder, true);
+    function addUser(address who) auth {
+        setUserRole(who, user, true);
     }
 
     function delAdmin(address who) auth {
         setUserRole(who, admin, false);
     }
-    function delIssuer(address who) auth {
-        setUserRole(who, issuer, false);
-    }
-    function delHolder(address who) auth {
-        setUserRole(who, holder, false);
+    function delUser(address who) auth {
+        setUserRole(who, user, false);
     }
 
     function isAdmin(address who) constant returns (bool) {
         return hasUserRole(who, admin);
     }
-    function isIssuer(address who) constant returns (bool) {
-        return hasUserRole(who, issuer);
-    }
-    function isHolder(address who) constant returns (bool) {
-        return hasUserRole(who, holder);
+    function isUser(address who) constant returns (bool) {
+        return hasUserRole(who, user);
     }
 }
