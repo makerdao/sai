0a0320660073540bd5ce8e97fc885197b6688451
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-06-20 13:49:15 -0300

    Adding tip.par in lpc

--- src/lpc.sol
+++ src/lpc.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.4.11;
 import "ds-thing/thing.sol";
 import "ds-token/token.sol";
 import "ds-value/value.sol";
+import "./tip.sol";
 
 
 contract SaiLPC is DSThing {
@@ -31,13 +32,15 @@ contract SaiLPC is DSThing {
     DSValue  public  pip;  // price feed, giving refs per alt
     uint128  public  gap;  // spread, charged on `take`
     DSToken  public  lps;  // 'liquidity provider shares', earns spread
+    Tip      public  tip;
 
-    function SaiLPC(ERC20 ref_, ERC20 alt_, DSValue pip_, DSToken lps_) {
+    function SaiLPC(ERC20 ref_, ERC20 alt_, DSValue pip_, DSToken lps_, Tip tip_) {
         ref = ref_;
         alt = alt_;
         pip = pip_;
 
         lps = lps_;
+        tip = tip_;
         gap = WAD;
     }
 
@@ -54,7 +57,7 @@ contract SaiLPC is DSThing {
     // total pool value
     function pie() constant returns (uint128) {
         return wadd(uint128(ref.balanceOf(this)),
-                    wmul(uint128(alt.balanceOf(this)), tag()));
+                    wdiv(wmul(uint128(alt.balanceOf(this)), tag()), tip.par()));
     }
 
     // lps per ref
@@ -68,7 +71,7 @@ contract SaiLPC is DSThing {
     function pool(ERC20 gem, uint128 wad) auth note {
         require(gem == alt || gem == ref);
 
-        var jam = (gem == ref) ? wad : wmul(wad, tag());
+        var jam = (gem == ref) ? wad : wdiv(wmul(wad, tag()), tip.par());
         var ink = rmul(jam, per());
         lps.mint(ink);
         lps.push(msg.sender, ink);
@@ -80,7 +83,7 @@ contract SaiLPC is DSThing {
     function exit(ERC20 gem, uint128 wad) auth note {
         require(gem == alt || gem == ref);
 
-        var jam = (gem == ref) ? wad : wmul(wad, tag());
+        var jam = (gem == ref) ? wad : wdiv(wmul(wad, tag()), tip.par());
         var ink = rmul(jam, per());
         // pay fee to exit, unless you're the last out
         ink = (jam == pie())? ink : wmul(gap, ink);
@@ -96,7 +99,7 @@ contract SaiLPC is DSThing {
     function take(ERC20 gem, uint128 wad) auth note {
         require(gem == alt || gem == ref);
 
-        var jam = (gem == ref) ? wdiv(wad, tag()) : wmul(wad, tag());
+        var jam = (gem == ref) ? wmul(wdiv(wad, tag()), tip.par()) : wdiv(wmul(wad, tag()), tip.par());
         jam = wmul(gap, jam);
 
         var pay = (gem == ref) ? alt : ref;
