07d8a933278269bafa8dad75e6b4b8868c39f552
rain <rainbreak@riseup.net>
2017-09-12 16:52:10 +0100

    specific tip tests

---
+++ src/tip.t.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.13;
+
+import "ds-test/test.sol";
+import "ds-math/math.sol";
+import './tip.sol';
+
+contract TipTest is DSTest, DSMath {
+    SaiTip tip;
+    function setUp() {
+        tip = new SaiTip();
+    }
+    function testTipDefaultPar() {
+        assertEq(tip.par(), WAD);
+    }
+    function testTipDefaultWay() {
+        assertEq(tip.way(), RAY);
+    }
+    function testTipCoax() {
+        tip.coax(999999406327787478619865402);  // -5% / day
+        assertEq(tip.way(), 999999406327787478619865402);
+    }
+    function testTipProd() {
+        tip.coax(999999406327787478619865402);  // -5% / day
+        tip.prod();
+    }
+    function testTipProdAfterWarp1day() {
+        tip.coax(999999406327787478619865402);  // -5% / day
+        tip.warp(1 days);
+        tip.prod();
+    }
+    function testTipParAfterWarp1day() {
+        tip.coax(999999406327787478619865402);  // -5% / day
+        tip.warp(1 days);
+        assertEq(tip.par(), 0.95 ether);
+    }
+    function testTipProdAfterWarp2day() {
+        tip.coax(999991977495368425989823173);  // -50% / day
+        tip.warp(2 days);
+        assertEq(tip.par(), 0.25 ether);
+    }
+}
