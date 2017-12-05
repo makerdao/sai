bcf01854eb5830aee1f49877d553d6e975b8f0e8
Nikolai Mushegian <nikolai@nexusdev.us>
2017-12-05 18:19:17 -0500

    spell test

--- src/spell.t.sol
+++ src/spell.t.sol
@@ -1,11 +1,13 @@
 // (c) Nikolai Mushegian, 2017
 
-// Testing using a `DSSpell` as the `hat` in a `DSChief` for one-off role
+// Testing using a `DSSpell` as the `hat` in a `DSChief` for one-off
+// root calls or role changes.
 
 import 'ds-test/test.sol';
 
 import 'ds-spell/spell.sol';
 import 'ds-chief/chief.sol';
+import 'ds-token/token.sol';
 
 contract Target is DSThing {
     bool public ouch;
@@ -19,6 +21,19 @@ contract SpellTest is DSTest {
     DSChief c;
     DSSpell s;
     function setUp() {
+        var gov = new DSToken("GOV");
+        var iou = new DSToken("IOU");
         t = new Target();
+        c = new DSChief(gov, iou, 1);
+    }
+    function testRootCall() {
+        // poke() sig: 0x18178358
+        bytes data = [0x18, 0x17, 0x83, 0x58];
+        s = new Spell(t, 0, data);
+    }
+    function testRoleChange() {
+        require(false);
+        // setUserRole(address,uint8,bool) sig: 
+        // TODO complex packing
     }
 }
